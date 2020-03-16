18dd175e07af1c1381db417e88c59bbe7185b4bd
skradha26 <skradha26@gmail.com>
2020-03-16 12:06:31 -0400

    Updated script.py
    Updated script.py with check for gopath.

--- script.py
+++ script.py
@@ -3,9 +3,11 @@
 import subprocess
 import os
 import sys
+import pathlib
+
 
 '''
-Script follows the steps in Part 1 of https://github.com/crcresearch/evm-fabric-setup till step "Get Fabric Samples and download Fabric images"
+Script follows the steps in Part 1 of https://github.com/crcresearch/evm-fabric-setup
 '''
 
 def dockertestfunc():
@@ -13,8 +15,7 @@ def dockertestfunc():
     print(dockerimageslist)
     if dockerimageslist == 0:
         print("No dangling hyperledger docker images found")
-        proceed_yes = input("Proceeding with download and installation.. Press ENTER to continue..")
-        corefunc()
+
 
     output = subprocess.call('''docker rmi -f $(docker images | grep hyperledger | awk '/^.*\s/ {print $3}') ''',stderr=subprocess.DEVNULL,shell=True)
     print("output \n" , output)
@@ -23,20 +24,25 @@ def dockertestfunc():
         subprocess.call('''docker rmi -f $(docker images | grep hyperledger | awk '/^.*\s/ {print $3}') ''', shell=True)  
 
     else:
+        corefunc()           
 
-        corefunc()
 
 def corefunc():
-
-    home = os.getenv("HOME")
-    gopath = home + "/.go"
-    print("Path to GO folder", gopath)
-    os.mkdir(gopath)
-    addPath = os.getenv("PATH") + ":" + "/" + gopath
-    print("GOPATH will be added to your PATH \n\n")
-    print(addPath)
-    os.environ['PATH']=addPath
-    path1 = os.getenv("PATH")
+    print("Checking Go path..")
+    output = str(os.system("echo $PATH | grep -q \"/Users/sradha/.go\" && echo \"go path exists\""))
+    if output != "found it":
+        home = os.getenv("HOME")
+        gopath = home + "/.go"
+        print("Path to GO folder", gopath)
+        os.mkdir(gopath)
+        addPath = os.getenv("PATH") + ":" + "/" + gopath
+        print("GOPATH will be added to your PATH \n\n")
+        print(addPath)
+        os.environ['PATH']=addPath
+        path1 = os.getenv("PATH")
+    else:
+        proceed_yes = input("Go path exists")  
+    
     user_input = input("Proceeding to get Hyperledger Fabric Samples and Download Fabric Images.. Press ENTER to continue..")
     print("Hyperledger directory will be created in the path: $GOPATH/src/github.com/hyperledger \n\n")
     os.makedirs(gopath + "/src/github.com/hyperledger")
